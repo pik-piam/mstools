f750fa6a4939f591af1e253425c60d592196e6be
Michael Crawford <mscrawfor@gmail.com>
2025-05-29 19:11:17 +0200

    follow-up bugfix

--- R/toolFertilizerDistribution.R
+++ R/toolFertilizerDistribution.R
@@ -2,16 +2,21 @@
 #' @description Disaggregates fertilizer usage to best match soil nitrogen uptake efficiency (snupe).
 #' @param iterMax Maximum number of iterations for downscaling
 #' @param maxSnupe Maximum allowed snupe (used to cap NA's during iteration)
+#' @param mapping mapping used for disaggregation
+#' @param from name of from column in mapping
+#' @param to name of to column in mapping
 #' @param fertilizer Total inorganic fertilizer to distribute (regional level)
 #' @param snupe Regional soil nitrogen uptake efficiency
 #' @param withdrawals Nitrogen withdrawals at the cell level
 #' @param organicinputs Non-inorganic fertilizer inputs at the cell level
 #' @param threshold Tg N difference below which we consider convergence "good enough"
 #' @return magpie object of fertilizer usage at cell level
-#' @importFrom magclass getYears getRegions dimSums
+#' @author Benjamin Leon Bodirsky, Michael Crawford
+#' @importFrom magclass getYears dimSums
 #'
 #' @export
 toolFertilizerDistribution <- function(iterMax = 50, maxSnupe = 0.85, fertilizer,
+                                       mapping = NULL, from = NULL, to = NULL,
                                        snupe, withdrawals, organicinputs, threshold = 0.5) {
 
   # - harmonize years
@@ -22,7 +27,14 @@ toolFertilizerDistribution <- function(iterMax = 50, maxSnupe = 0.85, fertilizer
   }
 
   # - aggregate to region
-  withdrawalsReg <- dimSums(withdrawals, dim = c(1.1, 1.2))
+  # this function will either work with simple `celliso` (e.g. -109p25.58p75.CAN) or
+  # with a mapping file when it's provided. The clusterfile used by magpie4 is particularly relevant
+  if (!is.null(mapping)) {
+    withdrawalsReg <- toolAggregate(withdrawals, dim = 1, rel = mapping, from = from, to = to, partrel = TRUE)
+  } else {
+    withdrawalsReg <- dimSums(withdrawals, dim = c(1.1, 1.2))
+  }
+
   regions <- getItems(withdrawalsReg, dim = 1)
   snupe <- snupe[regions, , ]
   fertilizerRegional <- fertilizer[regions, , ]
@@ -32,8 +44,14 @@ toolFertilizerDistribution <- function(iterMax = 50, maxSnupe = 0.85, fertilizer
   for (i in seq_len(iterMax)) {
     message(sprintf("Iteration %d/%d", i, iterMax))
 
+    if (!is.null(mapping)) {
+      snupeDisagg <- toolAggregate(snupe, rel = mapping, from = to, to = from, partrel = TRUE)
+    } else {
+      snupeDisagg <- snupe
+    }
+
     # - compute required inputs per cell
-    required <- withdrawals / snupe
+    required <- withdrawals / snupeDisagg
     required[is.nan(required)] <- 0
 
     # - split organic into usable vs. excessive
@@ -49,7 +67,12 @@ toolFertilizerDistribution <- function(iterMax = 50, maxSnupe = 0.85, fertilizer
     }
 
     # - update snupe based on new regional balance
-    usableOrganicRegional <- dimSums(usableOrganic, dim = c(1.1, 1.2))
+    if (!is.null(mapping)) {
+      usableOrganicRegional <- toolAggregate(usableOrganic, dim = 1, rel = mapping,
+                                             from = from, to = to, partrel = TRUE)
+    } else {
+      usableOrganicRegional <- dimSums(usableOrganic, dim = c(1.1, 1.2))
+    }
     snupe <- withdrawalsReg / (usableOrganicRegional + fertilizerRegional)
     snupe[is.na(snupe)] <- maxSnupe
   }
