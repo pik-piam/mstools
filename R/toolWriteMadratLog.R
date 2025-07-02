c92ae654667d268d3971a294e7cb627482850b63
Patrick R <hi@patrickrein.de>
2025-07-02 15:36:24 +0200

    Adds to toolWriteMadratLog documentation that log is written in YAML

--- R/toolWriteMadratLog.R
+++ R/toolWriteMadratLog.R
@@ -1,7 +1,7 @@
 #' toolWriteMadratLog
 #'
-#' Tool function for writing madrat messages to a log file. Useful
-#' after running madrat calculations which are performing checks via
+#' Tool function for writing madrat messages to a log file in the YAML format.
+#' Useful after running madrat calculations which are performing checks via
 #' \code{\link{toolExpectTrue}} or other toolExpect functions.
 #' @param checkResults list of check results as returned by \code{\link[madrat]{getMadratMessage}}
 #' @param logPath path to the log file to be written
